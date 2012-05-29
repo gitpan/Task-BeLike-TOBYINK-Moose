3a8f445b7c3af1e554323a1f33fc3946e42bae4a
Toby Inkster <tobyink@cpan.org>
2012-05-29 22:23:14 +0000

    Import of TOBYINK/Task-BeLike-TOBYINK-Moose-2012.150 from CPAN.
    gitpan-cpan-distribution: Task-BeLike-TOBYINK-Moose
    gitpan-cpan-version:      2012.150
    gitpan-cpan-path:         TOBYINK/Task-BeLike-TOBYINK-Moose-2012.150.tar.gz
    gitpan-cpan-author:       TOBYINK
    gitpan-cpan-maturity:     released

---
+++ lib/Task/BeLike/TOBYINK/Moose.pm
@@ -0,0 +1,75 @@
+package Task::BeLike::TOBYINK::Moose;
+
+use 5.010;
+
+BEGIN {
+	$Task::BeLike::TOBYINK::Moose::AUTHORITY = 'cpan:TOBYINK';
+	$Task::BeLike::TOBYINK::Moose::VERSION   = '2012.150';
+}
+
+__PACKAGE__
+__END__
+
+=head1 NAME
+
+Task::BeLike::TOBYINK::Moose - be like TOBYINK and use these Moose stuffz
+
+=head1 DESCRIPTION
+
+Task installs the following modules:
+
+=over
+
+=item * Any::Moose
+
+=item * Moose
+
+=item * MooseX::Declare
+
+=item * MooseX::NonMoose
+
+=item * MooseX::Semantic
+
+=item * MooseX::Traits
+
+=item * MooseX::Types
+
+=item * MooseX::Types::Common
+
+=item * MooseX::Types::DateTime::ButMaintained
+
+=item * MooseX::Types::Path::Class
+
+=item * MooseX::Types::Perl
+
+=item * MooseX::Types::URI
+
+=item * Throwable
+
+=item * TryCatch
+
+=item * namespace::sweep
+
+=back
+
+=head1 SEE ALSO
+
+L<Acme::About::TOBYINK>,
+L<Task::BeLike::TOBYINK>.
+
+=head1 AUTHOR
+
+Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENCE
+
+This software is copyright (c) 2012 by Toby Inkster.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=head1 DISCLAIMER OF WARRANTIES
+
+THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
